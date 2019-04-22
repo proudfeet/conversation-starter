82e9a13ff9f57a8b182b61f9b1b0b18d4e8f48ca
Simon Proudfoot <simon.d.proudfoot@gmail.com>
2019-04-21 20:43:11 -0400

    Updates

---
+++ precache-manifest.d9c3b6582e11d3a983087087b87f2a77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "feb5d458cd0b0f2c913a",
+    "url": "/conversation-starter/static/js/runtime~main.7b82ab15.js"
+  },
+  {
+    "revision": "319e2518c91fa6f4e3a8",
+    "url": "/conversation-starter/static/js/main.a1736497.chunk.js"
+  },
+  {
+    "revision": "dc9fbd5c0ecc2c2bd50b",
+    "url": "/conversation-starter/static/js/2.f1726665.chunk.js"
+  },
+  {
+    "revision": "319e2518c91fa6f4e3a8",
+    "url": "/conversation-starter/static/css/main.9194eb79.chunk.css"
+  },
+  {
+    "revision": "661166b6502b369bab138e85a350817d",
+    "url": "/conversation-starter/index.html"
+  }
+];
